7eb54a94c5496c4dd325de373fd9ce4ca974b140
muschellij2 <muschellij2@gmail.com>
2021-06-22 12:25:43 -0400

    set_knitr

---
+++ R/set_knitr_image_path.R
@@ -0,0 +1,16 @@
+
+#' Set image path for `knitr`
+#'
+#' @param verbose print out what the figure path is
+#'
+#' @return NULL
+#' @export
+set_knitr_image_path = function(verbose = FALSE) {
+  fp <- knitr::fig_path()
+  fp <- dirname(fp)
+  fp <- paste0("images/", fp, "/")
+  if (verbose) {
+    message(paste0("figpath is ", fp))
+  }
+  knitr::opts_chunk$set(fig.path = fp)
+}
