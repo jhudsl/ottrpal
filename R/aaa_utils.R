1a9160ef51718403b782dc9cc5679fd56982ac75
Candace Savonen <cansav09@gmail.com>
2021-09-03 18:02:58 -0400

    Add global variables

--- R/aaa_utils.R
+++ R/aaa_utils.R
@@ -1,4 +1,7 @@
 
+utils::globalVariables(c("num’", "quiz_dir", "type_url", "file_name"))
+
+
 # get script path and number of paragraphs
 paragraph_from_script <- function(x) {
   if (file.exists(x)) {
