9f62cf022570d09d59d102c95b78deae35ee275a
Candace Savonen <cansav09@gmail.com>
2021-09-28 09:54:38 -0400

    Update example vignette

--- R/example_data.R
+++ R/example_data.R
@@ -35,62 +35,52 @@ bad_quiz_path <- function() {
 #' @param dest_dir The destination directory you would like the example repo files to be placed. By default is current directory.
 #' @export
 #'
-#' @importFrom utils download.file
-#' @importFrom utils unzip
+#' @importFrom fs dir_copy
 #' @examples
 #'
 #' example_repo_setup()
 example_repo_setup <- function(dest_dir = ".") {
 
+  bookdown_path <- list.files(
+    pattern = "_bookdown.yml$",
+    system.file("extdata/", package = "leanbuild"),
+    full.names = TRUE
+  )
+
+  # Copy over whole directory
+  fs::dir_copy(dirname(bookdown_path), dest_dir, overwrite = TRUE)
+
+  copied_files <- list.files(dirname(bookdown_path), full.names = TRUE)
+
+  return(copied_files)
 }
 
 #' Clean up example repo files
 #'
-#' @param dir What directory to delete the example files from
+#' @param files_to_remove List of example files to delete.
 #' @export
 #'
 #' @examples
 #'
 #' example_repo_cleanup()
 #'
-example_repo_cleanup <- function(dir = ".") {
-
-  file_list <- file.path(dir, "resources", "needed_leanbuild_files.txt")
-  if (file.exists(file_list)) {
-    file_list <- readLines(file.path(dir, "resources", "needed_leanbuild_files.txt"))
-  } else {
-    file_list <- NULL
-  }
-  # Find example folder file
-  files_to_remove <- c(
-    file_list,
-    "_bookdown.yml",
-    "_output.yml",
-    "01-intro.Rmd",
-    "02-chapter_of_course.Rmd",
-    "About.Rmd",
-    "assets",
-    "docs",
-    "example-repo.zip",
-    "index.Rmd",
-    "quizzes",
-    "resources",
-    "Course_Name.rds",
-    "manuscript",
-    "question_error_report.tsv",
-    "packages.bib")
-
+example_repo_cleanup <- function(files_to_remove, verbose = FALSE) {
 
   message("Cleaning up and removing example repo files")
 
-  # Now remove it all
-  lapply(files_to_remove, function(file) {
-    if (file.exists(file) | dir.exists(file)) {
-      system(paste0("chmod +w -R", file))
-      system(paste0("sudo rm -r ", file))
+  files_to_remove <- grep(c("^docs$|^manuscript$|^resources$|^extdata$"),
+                          basename(files), value = TRUE, invert = TRUE)
+  files_to_remove <- c(files_to_remove,
+                       list.files("docs", recursive = TRUE, full.names = TRUE),
+                       list.files("manuscript",  recursive = TRUE, full.names = TRUE),
+                       list.files("resources", recursive = TRUE, full.names = TRUE)
+  )
+
+  lapply(files_to_remove, function(file2remove, verbose = verbose) {
+    if (file.exists(file2remove)) {
+      message(paste0("Removing: ", file2remove))
+      file.remove(file2remove)
     }
   })
-}
 
-# save(bad_quiz, bad_quiz, file = "bad_quiz.RData")
-# save(good_quiz, good_quiz, file = "good_quiz.RData")
+}
