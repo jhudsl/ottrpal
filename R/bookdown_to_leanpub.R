3ac4471fe4c0474e04dd6adf74170cb534a4917f
Candace Savonen <cansav09@gmail.com>
2022-02-04 10:58:33 -0500

    Add embed option

--- R/bookdown_to_leanpub.R
+++ R/bookdown_to_leanpub.R
@@ -1,170 +1,4 @@
 
-#' Load in Bookdown specifications from _bookdown.yml
-#'
-#' @param path  Where to look for the _bookdown.yml file. Passes to the bookdown_file() function. By default looks in current directory
-#'
-#' @return The yaml contents using yaml::yaml.load_file()
-#' @export
-
-get_bookdown_spec <- function(path = ".") {
-
-  # Get the file path to _bookdown.yaml
-  file_path <- bookdown_file(path = path)
-
-  # Read in yaml
-  suppressWarnings({
-    yaml_contents <- yaml::yaml.load_file(file_path)
-  })
-
-  return(yaml_contents)
-}
-
-#' Find main Bookdown directory
-#'
-#' @param path  Where to look for the file. By default looks in current directory.
-#'
-#' @return Returns the directory where the _bookdown.yml is contained.
-#' @export
-
-bookdown_path <- function(path = ".") {
-
-  # See what unzip is being used
-  operating_system <- Sys.info()[1]
-
-  path <- rprojroot::find_root(rprojroot::has_file("_bookdown.yml"), path = file.path(path))
-
-  return(path)
-}
-
-#' Find file path to _bookdown.yml
-#'
-#' @param path  Where to look for the _bookdown.yml file. Passes to the bookdown_file() function. By default looks in current directory
-#'
-#' @return The file path to _bookdown.yml
-#' @export
-#'
-bookdown_file <- function(path = ".") {
-  root_dir <- bookdown_path(path = file.path(path))
-  file_path <- file.path(root_dir, "_bookdown.yml")
-
-  return(file_path)
-}
-
-#' Get file paths all Rmds in the bookdown directory
-#'
-#' @param path  Where to look for the _bookdown.yml file. Passes toget_bookdown_spec() function. By default looks in current directory
-#'
-#' @return The file paths to Rmds listed in the _bookdown.yml file.
-#' @export
-#'
-bookdown_rmd_files <- function(path = ".") {
-  spec <- get_bookdown_spec(file.path(path))
-
-  files <- spec$rmd_files
-  if (is.null(files) || all(is.na(files)) || length(files) == 0) {
-    warning(
-      "No bookdown specification of the files, using ",
-      "list.files(pattern ='.Rmd')"
-    )
-    root_dir <- bookdown_path(path = file.path(path))
-    files <- list.files(
-      pattern = "[.]Rmd", ignore.case = TRUE,
-      path = root_dir, full.names = FALSE
-    )
-  }
-  return(files)
-}
-
-#' Declare file path to docs/ folder
-#'
-#' @param path  Where to look for the _bookdown.yml file. Passes toget_bookdown_spec() function. By default looks in current directory
-#'
-#' @return The file paths to Rmds listed in the _bookdown.yml file.
-#' @export
-#'
-bookdown_destination <- function(path = ".") {
-
-  # Find _bookdown.yml
-  root_dir <- bookdown_path(path = file.path(path))
-
-  # Get specs from _bookdown.yml
-  spec <- get_bookdown_spec(path = file.path(path))
-
-  # Find output directory declared in the bookdown.yml
-  output_dir <- spec$output_dir
-
-  # If none specified, assume its called docs/
-  if (is.null(output_dir)) {
-    output_dir <- "docs"
-  }
-  # Get the full file path
-  full_output_dir <- file.path(root_dir, output_dir)
-
-  # If the output dir doesn't exist, make it
-  dir.create(full_output_dir, showWarnings = FALSE, recursive = TRUE)
-
-  # Declare full paths
-  full_output_dir <- normalizePath(full_output_dir, winslash = "/")
-
-  return(full_output_dir)
-}
-
-copy_resources <- function(path = ".",
-                           images_dir = "resources",
-                           output_dir = "manuscript") {
-
-  # Get file path to bookdown.yml
-  path <- bookdown_path(path)
-
-  # Assume image directory is `resources/images`
-  res_image_dir <- file.path(path, images_dir)
-
-  # Create the directory if it doesn't exist
-  dir.create(res_image_dir, showWarnings = FALSE, recursive = TRUE)
-
-  manuscript_image_dir <- file.path(output_dir, images_dir)
-
-  dir.create(manuscript_image_dir, showWarnings = FALSE, recursive = TRUE)
-
-  manuscript_image_dir <- normalizePath(manuscript_image_dir)
-
-  if (dir.exists(res_image_dir)) {
-    R.utils::copyDirectory(res_image_dir, manuscript_image_dir, recursive = TRUE, overwrite = TRUE)
-  }
-}
-
-copy_docs <- function(path = ".", output_dir = "manuscript") {
-  path <- bookdown_destination(path)
-  R.utils::copyDirectory(path, file.path(output_dir), recursive = TRUE, overwrite = TRUE)
-}
-
-copy_bib <- function(path = ".", output_dir = "manuscript") {
-  path <- bookdown_path(path)
-  files <- list.files(path = path, full.names = TRUE, pattern = ".bib$")
-  if (length(files) > 0) {
-    file.copy(files, file.path(output_dir), overwrite = TRUE)
-  }
-}
-
-copy_quizzes <- function(quiz_dir = "quizzes", output_dir = "manuscript") {
-  quiz_dir <- file.path(quiz_dir)
-
-  if (!dir.exists(quiz_dir)) {
-    stop(paste(
-      "The quiz directory specified by quiz_dir:", quiz_dir, "does not exist.",
-      "If you don't have quizzes, set quiz_dir = NULL"
-    ))
-  }
-  quizzes <- list.files(path = file.path(quiz_dir), full.names = TRUE, pattern = "\\.md$")
-  if (length(quizzes) > 0) {
-    fs::file_copy(quizzes, file.path(output_dir, basename(quizzes)),
-      overwrite = TRUE
-    )
-  }
-}
-
-
-
 #' Convert Bookdown to Leanpub
 #'
 #' @param path path to the bookdown book, must have a `_bookdown.yml` file
@@ -195,69 +29,7 @@ bookdown_to_leanpub <- function(path = ".",
                                 remove_resources_start = FALSE,
                                 verbose = TRUE,
                                 footer_text = NULL) {
-
-  # Declare regex for finding rmd files
-  rmd_regex <- "[.][R|r]md$"
-
-  # Get the path to the _bookdown.yml
-  path <- bookdown_path(path)
-
-  if (verbose) {
-    message(paste0("Looking for bookdown file in ", path))
-  }
-  rmd_files <- bookdown_rmd_files(path = path)
-
-  if (verbose) {
-    message(paste0(c("Processing these files: ", rmd_files), collapse = "\n"))
-  }
-
-  if (render) {
-    if (verbose) {
-      message("Rendering the Book")
-    }
-    # Get the index file path
-    index_file <- grep("index", rmd_files, ignore.case = TRUE, value = TRUE)
-
-    index_file <- normalizePath(index_file)
-
-    if (length(index_file) == 0 || is.na(index_file)) {
-      index_file <- rmd_files[1]
-    }
-    message(paste("index_file is", index_file))
-
-    if (rmarkdown::pandoc_version() >= "2.11") {
-      output_format <- bookdown::gitbook(pandoc_args = "--citeproc")
-      output_format$pandoc$args <- c(output_format$pandoc$args, "--citeproc")
-    } else {
-      warning("Pandoc version is not greater than 2.11 so citations will not be able to be rendered properly")
-      output_format <- NULL
-    }
-    bookdown::render_book(
-      input = index_file,
-      output_format = output_format,
-      clean_envir = FALSE
-    )
-  }
-
-  # may be irrelevant since copy_docs does everything
-  if (verbose) {
-    message("Copying Resources")
-  }
-  copy_resources(path, output_dir = output_dir)
-
-  if (verbose) {
-    message("Copying Docs folder")
-  }
-
-  copy_docs(path, output_dir = output_dir)
-  if (verbose) {
-    message("Copying docs files")
-  }
-
-  copy_bib(path, output_dir = output_dir)
-  if (verbose) {
-    message("Copying bib files")
-  }
+  set_up_leanpub(...)
 
   bib_files <- list.files(pattern = "[.]bib$")
   if (length(bib_files) > 0) {
@@ -291,23 +63,7 @@ bookdown_to_leanpub <- function(path = ".",
       )
     }
   }
-  if (!is.null(quiz_dir)) {
-    #### Run quiz checks
-    if (run_quiz_checks) {
-      message("Checking quizzes")
-      quiz_checks <- check_quizzes(quiz_dir,
-        verbose = verbose
-      )
-    }
-    copy_quizzes(
-      quiz_dir = quiz_dir,
-      output_dir = output_dir
-    )
-    if (verbose) {
-      message("Copying quiz files")
-    }
-  }
-
+  ####################### Book.txt creation ####################################
   out <- NULL
   if (make_book_txt) {
     if (verbose > 1) {
@@ -338,6 +94,7 @@ bookdown_to_leanpub <- function(path = ".",
       ))
     }
   }
+  ############################# Wrapping up ####################################
   message(paste(
     "Leanpub ready files are saved to",
     output_dir,
@@ -345,6 +102,120 @@ bookdown_to_leanpub <- function(path = ".",
   ))
 }
 
+#' Convert Bookdown to Embed version of Leanpub
+#'
+#' @param path path to the bookdown book, must have a `_bookdown.yml` file
+#' @param chapt_img_key File path to a TSV whose contents are the chapter urls (`url`),
+#' the chapter titles (`chapt_title`), the file path to the image to be used for the chapter (`img_path`).
+#' Column names `url`, `chapt_title`, and `img_path` must be used.
+#' If no chapter title column supplied, the basename of the url will be used,
+#' If no image column supplied, default image used.
+#' @param default_img A google slide link to the default image to be used for all chapters
+#' @param output_dir output directory to put files.  It should likely be
+#' relative to path
+#' @param render if `TRUE`, then [bookdown::render_book()] will be run on each Rmd.
+#' @param verbose print diagnostic messages
+#' @param remove_resources_start remove the word `resources/` at the front
+#' of any image path.
+#' @param run_quiz_checks TRUE/FALSE run quiz checks
+#' @param make_book_txt Should [ottr::bookdown_to_book_txt()] be run
+#' to create a `Book.txt` in the output directory?
+#' @param quiz_dir directory that contains the quiz .md files that should be
+#' checked and incorporated into the Book.txt file. If you don't have quizzes,
+#' set this to NULL
+#' @param footer_text Optionally can add a bit of text that will be added to the
+#' end of each file before the references section.
+#'
+#' @return A list of output files and diagnostics
+#' @export
+#'
+bookdown_to_embed_leanpub <- function(path = ".",
+                                      chapt_img_key = NULL,
+                                      bookdown_index = file.path("docs", "index.html"),
+                                      base_url = NULL,
+                                      default_img = NULL,
+                                      render = TRUE,
+                                      output_dir = "manuscript",
+                                      make_book_txt = FALSE,
+                                      quiz_dir = "quizzes",
+                                      run_quiz_checks = FALSE,
+                                      remove_resources_start = FALSE,
+                                      verbose = TRUE,
+                                      footer_text = NULL) {
+  set_up_leanpub(...)
+
+  
+  if (!is.null(chapt_img_key)) {
+    message(paste("Reading in a chapt_img_key TSV file:", chapt_img_key))
+    readr::read_tsv(chapt_img_key)
+  } else {
+    message("Creating a chapt_img_key TSV file")
+    chapt_df <- get_chapters(bookdown_index = bookdown_index, 
+                             base_url = base_url)
+  }
+  
+  
+  # If there's no img_path supplied, then use a default image for each. 
+  if (!("img_path" %in% colnames(chapt_df))) {
+    if (is.null(default_img)) {
+      default_img <- "https://docs.google.com/presentation/d/1jEUxUY1qXDZ3DUtvTU6NCc6ASG5nx4Gwczv5aAglYX4/edit#slide=id.p"
+    }
+    # Set up location to store default image
+    img_dir <- file.path(output_dir, "embed_chapt_images")
+
+    if (!dir.exists(img_dir)) {
+      dir.create(img_dir, recursive = TRUE)
+    }
+    
+    # Download default image
+    chapt_df$img_path <- leanbuild::gs_png_download(url = default_img, output_dir = file.path(img_dir, "embed_chapt_images"), overwrite = TRUE)
+  }
+  
+  output_files <- chapt_df %>% 
+    # Make the data.frame be in the same order
+    dplyr::select(dplyr::any_of(url, chapt_title, img_path)) %>% 
+    # Run it make_embed_markdown on each row
+    purrr::pmap( ~ make_embed_markdown(url = ..1, chapt_title = ..2, img_path = ..3))
+
+  ####################### Book.txt creation ####################################
+  out <- NULL
+  if (make_book_txt) {
+    if (verbose > 1) {
+      message("Running bookdown_to_book_txt")
+    }
+    bookdown_to_book_txt(
+      path = path,
+      output_dir = output_dir,
+      quiz_dir = quiz_dir,
+      verbose = verbose
+    )
+    out <- book_txt_file <- file.path(output_dir, "Book.txt")
+  } else {
+    # If false, look for Book.txt file to copy to output folder.
+    book_txt_file <- file.path(path, "Book.txt")
+
+    if (file.exists(book_txt_file)) {
+      # Copy over an existing book.txt file if it exists
+      file.copy(from = book_txt_file, to = file.path(output_dir, "Book.txt"))
+
+      out <- book_txt_file <- file.path(output_dir, "Book.txt")
+    } else {
+      # If none exists and make_book_txt is false: stop.
+      stop(paste0(
+        "Book.txt file does not exist in the main directory: ", path, "and make_book_txt is set to FALSE.",
+        "There is no Book.txt file. Leanpub needs one. Either make one and place it in the directory path or ",
+        "use make_book_txt = TRUE and one will be generated for you."
+      ))
+    }
+  }
+
+  ############################# Wrapping up ####################################
+  message(paste(
+    "Leanpub ready files are saved to",
+    output_dir,
+    "Go to https://leanpub.com/ to publish them using the GitHub writing mode."
+  ))
+}
 
 #' Create Book.txt file from files existing in quiz directory
 #'
@@ -370,17 +241,17 @@ bookdown_to_book_txt <- function(path = ".",
   rmd_files <- bookdown_rmd_files(path = path)
 
   if (!is.null(quiz_dir)) {
-  # Find the quiz files in the quiz directory
-  quiz_files <- list.files(pattern = "\\.md$", quiz_dir)
+    # Find the quiz files in the quiz directory
+    quiz_files <- list.files(pattern = "\\.md$", quiz_dir)
 
-  # Put files in one vector
-  all_files <- c(rmd_files, quiz_files)
+    # Put files in one vector
+    all_files <- c(rmd_files, quiz_files)
 
-  # Make a vector specifying the file type: quiz or not
-  file_type <- c(
-    rep("non-quiz", length(rmd_files)),
-    rep("quiz", length(quiz_files))
-  )
+    # Make a vector specifying the file type: quiz or not
+    file_type <- c(
+      rep("non-quiz", length(rmd_files)),
+      rep("quiz", length(quiz_files))
+    )
   } else {
     all_files <- rmd_files
     file_type <- rep("non-quiz", length(rmd_files))
@@ -416,4 +287,93 @@ bookdown_to_book_txt <- function(path = ".",
   }
   # need to fix about quiz
   writeLines(all_files, con = book_txt)
+}
+
+
+#' Make Leanpub file that has embed webpage of a chapter
+#'
+#' @param url The url to the chapter that is to be embed
+#' @param chapt_name Title of chapter to be used as file name and printed on iframe
+#' @param img File path to image to use for poster
+#' @return A
+#'
+#' @export
+make_embed_markdown <- function(url,
+                                chapt_title,
+                                img_path,
+                                output_dir = "manuscript",
+                                verbose = TRUE) {
+  # Arguments:
+  #   url: The url to the chapter
+  #   chapt_name: The title of the chapter to be used as a header
+  #   img: file path to the image to be used in the preview
+  #
+  # Returns: A markdown document ready for Leanpub and the image copied to the manuscript folder
+
+  chapt_file_name <- gsub(" ", "-", chapt_title)
+
+  # Declare output file
+  output_file <- file.path(output_dir, paste0(chapt_file_name, ".md"))
+
+  file_contents <- c(
+    paste("#", chapt_title),
+    " ",
+    paste0("{type: iframe, title:", chapt_title, " width: 400, height: 400, poster:", img_path, "}"),
+    paste0("![](", url, ")")
+  )
+
+  write(file_contents, file = output_file)
+
+  manuscript_dir <- file.path(output_dir, dirname(img_path))
+
+  if (!dir.exists(manuscript_dir)) {
+    dir.create(manuscript_dir, recursive = TRUE)
+  }
+
+  file.copy(from = img_path, to = file.path(output_dir, img_path), overwrite = TRUE)
+
+  if (verbose) {
+    message(paste0("Output saved to: ", output_file))
+  }
+
+  return(output_file)
+}
+
+#' Make Leanpub file that has embed webpage of a chapter
+#'
+#' @param bookdown_index The file path of the rendered bookdown index.html file
+#' @param base_url The base url of where the chapters are published -- the url to provide to the iframe in Leanpub
+#' e.g. https://jhudatascience.org/OTTR_Template/coursera
+#' 
+#' @return A data.frame of the chapter urls and their titles that are to be ported to Leanpub. 
+#' This can be passed to 
+#'
+#' @export
+#'
+get_chapters <- function(bookdown_index = file.path("docs", "index.html"),
+                         base_url = NULL) {
+
+  # Read in html
+  index_html <- suppressWarnings(try(xml2::read_html(paste(bookdown_index, collapse = "\n"))))
+
+  # Extract chapter nodes
+  nodes <- rvest::html_nodes(index_html, xpath = paste0("//", 'li[@class="chapter"]'))
+
+  if (length(nodes) > 0) {
+    # Format into a data.frame
+    chapt_data <- rvest::html_attrs(nodes) %>%
+      dplyr::bind_rows() %>%
+      dplyr::rename_with(~ gsub("-", "_", .x, fixed = TRUE)) %>%
+      dplyr::mutate(
+        chapt_name = stringr::word(rvest::html_text(nodes), sep = "\n", 1),
+        url = paste0(base_url, data_path)
+      ) %>%
+      dplyr::select(url, chapt_title = chapt_name) %>%
+      as.data.frame() %>%
+      dplyr::distinct(url, .keep_all = TRUE)
+  } else {
+    stop(paste("Chapter retrieval from:", bookdown_index, "Failed."))
+  }
+
+  return(chapt_data)
 }
