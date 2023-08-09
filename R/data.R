e827f0e14ecff0e55cefe824e1a397b991a5af7d
howardbaek <howardbaek.fh@gmail.com>
2023-08-09 15:04:11 -0700

    Take out leftovers from rgoogleclassroom

--- R/data.R
+++ R/data.R
@@ -27,24 +27,3 @@ encrypt_creds_user_path <- function() {
     full.names = TRUE
   )
 }
-
-
-#' Get file path to an example quiz
-#'
-#' @return A file path to a markua markdown quiz example you can use for testing
-#' @export
-#' @examples \dontrun{
-#'
-#' # Find quiz path
-#'
-#' quiz_path <- markdown_quiz_path()
-#' }
-#'
-markdown_quiz_path <- function() {
-  list.files(
-    pattern = "quiz.md",
-    recursive = TRUE,
-    system.file("extdata", package = "rgoogleclassroom"),
-    full.names = TRUE
-  )
-}
