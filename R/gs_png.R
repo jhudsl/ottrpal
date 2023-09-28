7fc8f5ff1ce30bab74c0cd55720b3f1523d4371f
Howard Baek <50791792+howardbaek@users.noreply.github.com>
2023-09-28 10:26:31 -0700

    Change function names: `get_slide_id()` -> `get_presentation_id()`, `get_slide_page()` -> `get_slide_id()`

--- R/gs_png.R
+++ R/gs_png.R
@@ -1,4 +1,4 @@
-#' Get Slide ID from URL
+#' Get Presentation ID from URL
 #'
 #' @param x URL of slide
 #'
@@ -11,8 +11,8 @@
 #'   "1Tg-GTGnUPduOtZKYuMoelqUNZnUp3vvg_7TtpUPL7e8",
 #'   "/edit#slide=id.g154aa4fae2_0_58"
 #' )
-#' get_slide_id(x)
-get_slide_id <- function(x) {
+#' get_presentation_id(x)
+get_presentation_id <- function(x) {
   x <- sub(".*presentation/", "", x)
   x <- sub("/d/e", "/d", x) # if you publish by accident
   x <- sub("^(d|e)/", "", x)
@@ -35,15 +35,16 @@ get_slide_id <- function(x) {
 #'   "12DPZgPteQBwgal6kSPP58zhPhjZ7QSPZLe3NkA8M3eo/edit",
 #'   "#slide=id.gc8648f14c3_0_397&t=4"
 #' )
-#' id <- get_slide_id(url)
+#' id <- get_presentation_id(url)
 #' gs_png_url(url)
 gs_png_url <- function(url) {
-  id <- get_slide_id(url)
-  slide_id <- get_slide_page(url)
-  gs_png_id(id, slide_id)
+  presentation_id <- get_presentation_id(url)
+  slide_id <- get_slide_id(url)
+  gs_png_id(presentation_id, slide_id)
 }
 
-gs_png_id <- function(id, slide_id) {
+# Get URL to download slide as PNG
+gs_png_id <- function(presentation_id, slide_id) {
   if (any(grepl("^id[.]", slide_id))) {
     warning(
       "slide ids usually don't have format of id.gc*, ",
@@ -52,15 +53,16 @@ gs_png_id <- function(id, slide_id) {
   }
   paste0(
     "https://docs.google.com/presentation/d/",
-    id,
-    "/export/png?id=", id,
+    presentation_id,
+    "/export/png?id=", presentation_id,
     "&pageid=", slide_id
   )
 }
 
+#' Extract Slide ID from URL
 #' @export
 #' @rdname gs_png_url
-get_slide_page <- function(url) {
+get_slide_id <- function(url) {
   parsed <- httr::parse_url(url)
   slide_id <- parsed$query$pageid
   if (length(slide_id) == 0 || nchar(slide_id) == 0) {
@@ -73,16 +75,17 @@ get_slide_page <- function(url) {
   slide_id
 }
 
+#' Download a slide from Google Slides
 #' @export
 #' @rdname gs_png_url
 #' @param output_dir path to output png
 #' @param overwrite should the slide PNG be overwritten?
 gs_png_download <- function(url, output_dir = ".", overwrite = TRUE) {
-  id <- get_slide_id(url)
-  slide_id <- get_slide_page(url)
+  presentation_id <- get_presentation_id(url)
+  slide_id <- get_slide_id(url)
   url <- gs_png_url(url)
   dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
-  outfile <- file.path(output_dir, paste0(id, "_", slide_id, ".png"))
+  outfile <- file.path(output_dir, paste0(presentation_id, "_", slide_id, ".png"))
   if (!file.exists(outfile) || overwrite) {
     curl::curl_download(url, destfile = outfile, quiet = FALSE)
   }
@@ -97,6 +100,7 @@ gs_png_download <- function(url, output_dir = ".", overwrite = TRUE) {
 include_slide <- function(url,
                           output_dir = knitr::opts_chunk$get("fig.path"),
                           overwrite = TRUE, ...) {
+  alt_text <<- # extract alt text from slide
   outfile <- gs_png_download(url, output_dir, overwrite = overwrite)
   knitr::include_graphics(outfile, ...)
 }
