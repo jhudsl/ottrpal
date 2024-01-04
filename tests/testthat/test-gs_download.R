a8abfe0a8437466bba9345b24c7e84947e59fa38
Candace Savonen <cansav09@gmail.com>
2024-01-04 09:00:19 -0500

    Change slide

--- tests/testthat/test-gs_download.R
+++ tests/testthat/test-gs_download.R
@@ -3,9 +3,7 @@ testthat::test_that("Download a Slide ", {
   if (.Platform$OS.type != "windows") {
   output_dir = tempdir()
   outfile = ottrpal::include_slide(
-    paste0("https://docs.google.com/presentation/d/",
-           "12DPZgPteQBwgal6kSPP58zhPhjZ7QSPZLe3NkA8M3eo/",
-           "edit#slide=id.gc87451c247_0_17"),
+    "https://docs.google.com/presentation/d/1-7UvgVq5tP1pasTEErUM3bJFH2fU_pilH6i6_81CCXU/edit#slide=id.p",
     output_dir = output_dir,
     overwrite = FALSE)
   testthat::expect_true(file.exists(outfile))
