640ea6d5ce6ee131a18747610efb404f45c3fb32
Howard Baek <50791792+howardbaek@users.noreply.github.com>
2024-05-07 21:11:05 -0700

    Incorporate different style sets

--- R/setup_project.R
+++ R/setup_project.R
@@ -1,42 +1,44 @@
 setup_project <- function(path, ...) {
+  # collect inputs
+  dots <- list(...)
 
   # create directory
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
-  # create img folder within path
+  # create img folder within  path
   dir.create(paste0(path, "/img"), recursive = TRUE, showWarnings = FALSE)
   # create img/box-images folder within path
   dir.create(paste0(path, "/img/box-images"), recursive = TRUE, showWarnings = FALSE)
 
   # Move boilerplate files into path
 
-  # Vector of filenames to be copied (non-images)
-  boilerplate_file <- c("index.qmd", "intro.qmd", "404.qmd",
-                        "favicon.ico", "logo.png", "style.css",
-                        "references.bib", "_quarto.yml")
-
   # Function to copy each file
-  copy_files <- function(file_name) {
-    source_path <- system.file(paste0("ottr/", file_name), package = "ottrpal")
+  copy_files <- function(file_name, style_set) {
+    if (style_set == "FHDaSL") {
+      style_set <- "fhdasl"
+    } else if (style_set == "AnVIL") {
+      style_set <- "anvil"
+    } else {
+      style_set <- "gdscn"
+    }
+
+    source_path <- system.file(paste0("ottr/", style_set, "/",  file_name), package = "ottrpal")
     destination_path <- paste0(path, "/", file_name)
     file.copy(source_path, destination_path)
   }
 
-  # Apply the function to each file in the vector
-  lapply(boilerplate_file, copy_files)
 
   # Vector of filenames to be copied (non-images)
-  boilerplate_file_img <- c("img/logo.png", "img/box-images/note.png", "img/box-images/warning.png",
+  boilerplate_file <- c("index.qmd", "intro.qmd", "404.qmd",
+                       "style.css", "references.bib", "_quarto.yml")
+  # Apply the function to each file in the vector
+  lapply(boilerplate_file, copy_files, dots[[1]])
+
+
+  # Vector of filenames to be copied (images)
+  boilerplate_file_img <- c("img/logo.png", "img/favicon.ico",
+                            "img/box-images/note.png", "img/box-images/warning.png",
                             "img/box-images/github.png", "img/box-images/dictionary.png",
                             "img/box-images/thinking_face.png", "img/box-images/under_construction.png")
-
   # Apply the function to each file in the vector
-  lapply(boilerplate_file_img, copy_files)
-
-  # collect inputs
-  dots <- list(...)
-  text <- lapply(seq_along(dots), function(i) {
-    key <- names(dots)[[i]]
-    val <- dots[[i]]
-    paste0(key, ": ", val)
-  })
+  lapply(boilerplate_file_img, copy_files, dots[[1]])
 }
