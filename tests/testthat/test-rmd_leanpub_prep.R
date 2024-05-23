f13cb1305e0b7a11fefac3e43375e0bca1d8b535
cansavvy <cansav09@gmail.com>
2024-05-23 10:35:50 -0400

    Style it

--- tests/testthat/test-rmd_leanpub_prep.R
+++ tests/testthat/test-rmd_leanpub_prep.R
@@ -1,28 +1,24 @@
+test_that("Tocless render for Rmd", {
+  # render_without_toc()
 
-test_that("Tocless render for Rmd") {
-
-# render_without_toc()
-
-# TODO: This should be functionalized and incorporated into the package
-# curl -o make_screenshots.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/make_screenshots.R
-#chapt_urls=$(Rscript --vanilla make_screenshots.R \
-#             --git_pat ${{ secrets.GH_PAT }} \
-#             --repo $GITHUB_REPOSITORY \
-#             --output_dir resources/chapt_screen_images)
-}
+  # TODO: This should be functionalized and incorporated into the package
+  # curl -o make_screenshots.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/make_screenshots.R
+  # chapt_urls=$(Rscript --vanilla make_screenshots.R \
+  #             --git_pat ${{ secrets.GH_PAT }} \
+  #             --repo $GITHUB_REPOSITORY \
+  #             --output_dir resources/chapt_screen_images)
+})
 
 
-test_that("Create Leanpub IFrames for Rmd"){
+test_that("Create Leanpub IFrames for Rmd", {
+  # ottrpal::bookdown_to_embed_leanpub(
+  #    render = FALSE, \
+  #    chapt_img_key = 'resources/chapt_screen_images/chapter_urls.tsv', \
+  #    make_book_txt = as.logical('${{needs.yaml-check.outputs.make_book_txt}}'), \
+  #    quiz_dir = NULL)
 
-# ottrpal::bookdown_to_embed_leanpub(
-#    render = FALSE, \
-#    chapt_img_key = 'resources/chapt_screen_images/chapter_urls.tsv', \
-#    make_book_txt = as.logical('${{needs.yaml-check.outputs.make_book_txt}}'), \
-#    quiz_dir = NULL)
-
-#ottrpal::bookdown_to_embed_leanpub(
-#            render = FALSE, \
-#            chapt_img_key = 'resources/chapt_screen_images/chapter_urls.tsv', \
-#            make_book_txt = as.logical('${{needs.yaml-check.outputs.make_book_txt}}'))
-
-}
+  # ottrpal::bookdown_to_embed_leanpub(
+  #            render = FALSE, \
+  #            chapt_img_key = 'resources/chapt_screen_images/chapter_urls.tsv', \
+  #            make_book_txt = as.logical('${{needs.yaml-check.outputs.make_book_txt}}'))
+})
