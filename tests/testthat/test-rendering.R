5143c2bbb144c8cccbd2733f39dfbb2c68942c87
Candace Savonen <cansav09@gmail.com>
2024-06-25 10:40:36 -0400

    Update rendering tests

--- tests/testthat/test-rendering.R
+++ tests/testthat/test-rendering.R
@@ -1,30 +1,36 @@
 test_that("Rmd Rendering", {
-  dir <- download_ottr_template(dir = "inst/extdata", type = "rmd")
+  dir <- download_ottr_template(dir = ".", type = "rmd")
 
   bookdown::render_book("index.Rmd", output_format = "all")
+  unlink(dir)
 })
 
 test_that("Quarto Rendering", {
-  dir <- download_ottr_template(dir = "inst/extdata", type = "quarto")
+  dir <- download_ottr_template(dir = ".", type = "quarto")
 
   quarto::quarto_render(dir)
   quarto::quarto_render(dir,
                         metadata = list(sidebar = F, toc = F),
                         quarto_args = c("--output-dir", "docs/no_toc/")
   )
+  unlink(dir)
 })
 
 test_that("Rmd Website Rendering", {
-  dir <- download_ottr_template(dir = "inst/extdata", type = "rmd")
+  dir <- download_ottr_template(dir = ".", type = "rmd")
 
   rmarkdown::clean_site(dir, preview = FALSE)
 
   rmarkdown::render_site(dir)
+  
+  unlink(dir)
 })
 
 
 test_that("Quarto Website Rendering", {
-  dir <- download_ottr_template(dir = "inst/extdata", type = "quarto")
+  dir <- download_ottr_template(dir = ".", type = "quarto")
 
   quarto::quarto_render(dir)
+  
+  unlink(dir)
 })
