d3cd9ccf700e310507caf92f1e0c98aecb2c72a1
Candace Savonen <cansav09@gmail.com>
2024-06-27 10:02:35 -0400

    suppressWarnings

--- tests/testthat/test-quizzes.R
+++ tests/testthat/test-quizzes.R
@@ -10,11 +10,19 @@ test_that("Quiz checks", {
   bad_quiz <- readLines(bad_quiz_path())
   bad_quiz_specs <- parse_quiz(bad_quiz)
 
+  # THe following checks *should fail* because we're giving it a bad quiz.
+  bad_quiz_checks <- suppressWarnings(check_all_questions(bad_quiz_specs))
+
   ## Make a temporary quiz directory
   quiz_dir <- dirname(good_quiz_path())
 
+  ## Now check the quizzes in that directory
+  # The following checks *should also fail* because the bad quiz is in there
+  all_quiz_results <- suppressWarnings(check_quizzes(quiz_dir = quiz_dir))
+
   ## TEST HERE:
   # 1. quiz_error_report.tsv should be made
-  # 2. good quiz should pass!
+  # 2. bad quiz should have errors
+  # 3. good quiz should pass!
 
 })
