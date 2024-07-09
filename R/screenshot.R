23f0fa5d74ae39c1ea16ab08e34ca189761783a5
Kate Isaac <41767733+kweav@users.noreply.github.com>
2024-07-09 11:52:10 -0400

    Update R/screenshot.R
    Co-authored-by: Candace Savonen <cansav09@gmail.com>

--- R/screenshot.R
+++ R/screenshot.R
@@ -2,7 +2,7 @@
 #' @description This function creates screenshots of course chapters that are stored in a created output directory 
 #' @param git_pat default is NULL; required argument; a Git secret
 #' @param repo default is NULL; required argument; GitHub repository name, e.g., jhudsl/OTTR_Template
-#' @param output_dir default is "resources/chapt_screen_images"; Output directory where the chapter's screen images should be stored
+#' @param output_dir default is "resources/chapt_screen_images"; Output directory where the chapter's screen images should be stored. For OTTR courses, don't change this unless you've changed the downstream functions accordingly. 
 #' @param base_url default is NULL; rendered bookdown URL where screenshots are taken from, if NULL, the function will use the repo_name and and git_pat to find the base_url
 #' @import cow
 #' @import dplyr
