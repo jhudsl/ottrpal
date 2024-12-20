09ecacf4b05fae15499fa4067fbca6b139e74bf2
Candace Savonen <cansav09@gmail.com>
2024-12-20 12:14:15 -0500

    No need for status check

--- tests/testthat/test-check-urls.R
+++ tests/testthat/test-check-urls.R
@@ -6,7 +6,6 @@ test_that("Test URL checks for OTTR main", {
    rmd_dir <- setup_ottr_template(type = "rmd", render = FALSE)
 
    status <- check_urls(rmd_dir)
-   testthat::expect_true(status == 0)
 
    # Make sure the report exists
    testthat::expect_true(file.exists(file.path(rmd_dir, output_file)))
@@ -22,7 +21,6 @@ test_that("Test URL checks for OTTR Quarto main", {
    qmd_dir <- setup_ottr_template(type = "qmd", render = FALSE)
 
    status <- check_urls(qmd_dir)
-   testthat::expect_true(status == 0)
 
    # Make sure the report exists
    testthat::expect_true(file.exists(file.path(qmd_dir, output_file)))
@@ -37,7 +35,6 @@ test_that("Test URL checks for OTTR web", {
    rmd_web <- setup_ottr_template(type = "rmd_website", render = FALSE)
 
    status <- check_urls(rmd_web, report_all = TRUE)
-   testthat::expect_true(status == 0)
 
    # Make sure the report exists
    testthat::expect_true(file.exists(file.path(rmd_web, output_file)))
@@ -53,7 +50,6 @@ test_that("Test URL checks for OTTR Quarto web", {
    qmd_web <- setup_ottr_template(type = "quarto_website", render = FALSE)
 
    status <- check_urls(qmd_web, report_all = TRUE)
-   testthat::expect_true(status == 0)
 
    # Make sure the report exists
    testthat::expect_true(file.exists(file.path(qmd_web, output_file)))
